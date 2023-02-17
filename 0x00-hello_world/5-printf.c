488cfb6ca64708b17b7c1f0446c2d9aee3640ffe
Elipatter <elliotpatterson36@gmail.com>
2023-02-17 06:44:33 -0800

    hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - A program that prints a statement follwed by a new line
- * Return: 0 (success)
+ * main - A program that prints followed by a line
+ *
+ * Return: 0 when succefull
  */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
