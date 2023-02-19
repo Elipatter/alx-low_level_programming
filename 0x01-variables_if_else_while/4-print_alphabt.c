81dac9a059e6e61e23b80d844e1ffdef79e0c31d
Elipatter <elliotpatterson36@gmail.com>
2023-02-19 08:33:10 -0800

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
 /**
  * main - a program that writes the alphabet in lowercase except
  *
  * Return: always 0
  */
-
 int main(void)
 {
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-			if (i != 'q' && i != 'e')
+		if (i != 'q' && i != 'e')
 			putchar(i);
 	putchar('\n');
 	return (0);
-}
-
