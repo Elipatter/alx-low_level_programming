cd5939a85c550f913a3c233570c115a2d9cd7327
Elipatter <elliotpatterson36@gmail.com>
2023-02-22 00:29:20 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * main - A c program that prints
- *
+ * main - A program that prints letters
+ * _putchhar: prints letters putchar
  *Return: always 0
  */
 
 int main(void)
 {
-	printf("__ptchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
